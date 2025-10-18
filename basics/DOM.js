a59539c48ec6b444a1576a3679ee9b7e4933c697
Ayansheikh1 <ayansheikh@ayans-MacBook-Air.local>
2025-10-18 23:55:17 +0530

    practice some concept

---
+++ basics/DOM.js
@@ -0,0 +1,18 @@
+//Create an object car with properties (brand, model, year) and a method getInfo().
+const car = {
+    brand: "BMW",
+    model: "M5",
+    year: 2025, 
+    getInfo: function(){
+        return `${this.brand} ${this.model} ${this.year}`;
+    }
+}
+
+console.log(car.getInfo())
+
+
+//Create an array of student objects and print all names
+
+const students = [
+    
+]
