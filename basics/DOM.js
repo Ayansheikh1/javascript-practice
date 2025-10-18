a827ef48d4113f10b5d5e331f03dfa6ba4cb95dd
Ayansheikh1 <ayansheikh@ayans-MacBook-Air.local>
2025-10-19 00:13:00 +0530

    create counter

--- basics/DOM.js
+++ basics/DOM.js
@@ -14,5 +14,8 @@ console.log(car.getInfo())
 //Create an array of student objects and print all names
 
 const students = [
-    
-]
+    {name: "ayan" , age: 23},
+    {name: "ashar", age: 19}, 
+    {name: "anam", age: 25}
+];
+students.forEach(student => console.log(student.name));
