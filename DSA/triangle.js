705fa9f7ff0795c6577b45812f90dd6214876a2e
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-15 22:22:24 +0530

    triangle printing

---
+++ DSA/triangle.js
@@ -0,0 +1,20 @@
+// Import prompt-sync package to take user input
+let prompt = require("prompt-sync")();
+
+// Take input from user and convert it into a Number
+let n = Number(prompt("Enter a number: "));
+
+// Outer loop → controls rows
+for (let i = 1; i <= n; i++) {
+
+    // Inner loop → controls columns
+     // j <= i means stars increase row by row
+    for (let j = 1; j <= i; j++) {
+
+        // Print "*" without moving to next line
+        process.stdout.write("*");
+    }
+
+    // Move cursor to next line after one row is printed
+    console.log();
+}
