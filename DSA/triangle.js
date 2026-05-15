a05fde537d3886dadb67392c5ea95ed0ced15ca6
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-15 22:29:44 +0530

    print number triangle

--- DSA/triangle.js
+++ DSA/triangle.js
@@ -18,3 +18,18 @@ for (let i = 1; i <= n; i++) {
     // Move cursor to next line after one row is printed
     console.log();
 }
+
+
+// Outer loop → controls rows
+for (let i = 1; i <= n; i++) {
+
+    // Inner loop → prints numbers from 1 to i
+    for (let j = 1; j <= i; j++) {
+
+        // Print numbers in same line
+        process.stdout.write(j + " ");
+    }
+
+    // Move to next line
+    console.log();
+}
