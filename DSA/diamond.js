db35388ebe5c157a4c401a80ba3495f5bd274bdd
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-16 17:03:40 +0530

    diamond pattern

---
+++ DSA/diamond.js
@@ -0,0 +1,25 @@
+let prompt = require("prompt-sync")();
+let n = prompt("enter a number");
+
+for(let i = 1; i <= n; i++){
+    for(let j = 1; j<=n-i; j++){
+        process.stdout.write(" ");
+    }
+    for(let j = 1; j<= 2*i-1;j++){
+        process.stdout.write("*");
+    }
+
+    console.log()
+}
+
+
+for(let i = n-1; i >= 1; i--){
+    for(let j = 1; j<=n-i; j++){
+        process.stdout.write(" ");
+    }
+    for(let j = 1; j<= 2*i-1;j++){
+        process.stdout.write("*");
+    }
+
+    console.log()
+}
