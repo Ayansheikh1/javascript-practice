42b5ac3a62c7f1fec0c0ef515da5d21313bc5aac
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-01 15:20:09 +0530

    refactor selection sort implementation for consistency and readability

--- DSA/array/selectionSort.js
+++ DSA/array/selectionSort.js
@@ -1,19 +1,18 @@
-let arr = [3,5,1,10,6];
+const arr = [3,5,1,10,6];
+let n = arr.length;
 
-let n= arr.length;
 for(let i = 0;i<n-1;i++){
     let minIndex = i;
-    for(let j =i+1; j<n;j++){
+    for(let j = i+1; j<n;j++){
         if(arr[j]<arr[minIndex]){
-            minIndex = j;
+            minIndex = j
         }
+
     }
-    if(minIndex!=i){
+    if(minIndex != i){
         let temp = arr[minIndex];
         arr[minIndex] = arr[i];
         arr[i] = temp;
     }
 }
-
-
 console.log(arr)
