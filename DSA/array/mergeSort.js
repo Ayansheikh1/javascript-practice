06ef3b76e981d6b5c460db25233553e92ee0a710
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-12 21:50:40 +0530

    add: implement merge sort algorithm in mergeSort.js

---
+++ DSA/array/mergeSort.js
@@ -0,0 +1,47 @@
+
+
+function conquor(arr, first, mid, last) {
+
+
+    let temp = new Array(last - first + 1);
+
+    let i = first;
+    let j = mid + 1;
+    let k = 0;
+
+    while (i <= mid && j <= last) {
+        if (arr[i] < arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++]
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= last) {
+        temp[k++] = arr[j++];
+
+    }
+
+    let p = 0, t = first;
+    while (p < temp.length) {
+        arr[t++] = temp[p++]
+    }
+
+}
+
+function divide(arr, first, last) {
+    if (first >= last) return
+    let mid = Math.floor((first + last) / 2);
+    divide(arr, first, mid);
+    divide(arr, mid + 1, last);
+    conquor(arr, first, mid, last);
+
+}
+
+let arr = [8, 2, 1, 5, 7, 9, 3];
+divide(arr, 0, arr.length - 1)
+console.log(arr)
