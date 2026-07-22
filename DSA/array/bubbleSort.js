17945be579f17b75f64d69c155798cd948ac1587
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-22 21:59:05 +0530

    practice bubble sort

--- DSA/array/bubbleSort.js
+++ DSA/array/bubbleSort.js
@@ -1,12 +1,14 @@
 let arr = [3,8,10,13,5,18];
 let n = arr.length;
-for(let i = 0; i<n-1; i++){
-    for(j=0;j<n-1-i;j++){
+for(let i = 0; i<n-1;i++){
+    for(let j = 0;j<=n-1-i;j++){
         if(arr[j]>arr[j+1]){
             let temp = arr[j];
-            arr[j]= arr[j+1];
+            arr[j] = arr[j+1];
             arr[j+1] = temp;
         }
     }
+
 }
-console.log(arr);
+
+console.log(arr)
