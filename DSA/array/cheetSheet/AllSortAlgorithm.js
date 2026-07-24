eee27d59d0aeb87e02c7a0ea155592ac8bf2f9cd
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-07-24 16:49:28 +0530

    add bubble sort implementation

--- DSA/array/cheetSheet/AllSortAlgorithm.js
+++ DSA/array/cheetSheet/AllSortAlgorithm.js
@@ -1,3 +1,38 @@
+//Bubble Sort
+let arr1 = [5, 3, 8, 1];
+let n = arr1.length;
+for(let i = 0; i<n-1;i++){ //outer loop for number of passes i<n-1 -- because after n-1 passes, the array will be sorted
+    for(let j = 0; j<n-1-i;j++){ //inner loop for comparing adjacent elements j<n-1-i -- because after each pass, the largest element is placed at the end of the array, so we can ignore the last i elements which are already sorted
+        if(arr1[j]>arr1[j+1]){//if the current element is greater than the next element, swap them
+            let temp = arr1[j];
+            arr1[j] = arr1[j+1];
+            arr1[j+1] = temp
+        }
+    }
+}
+console.log("bubble sort : ",arr1);
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Cyclic Sort
 let arr = [7,4,5,1,2,6,3]; //1 to n
 let i = 0; //initialize index to 0
@@ -12,4 +47,4 @@ while(i<arr.length){
         i++; //if the current element is at its correct index, move to the next index
     }
 }
-console.log(arr)
+console.log("cyclic sort: ",arr)
