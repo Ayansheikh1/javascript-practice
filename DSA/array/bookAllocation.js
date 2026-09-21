9cb86878fccaa5c39dbfe855348d0c6221bfa201
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-09-21 20:28:33 +0530

    feat: add initial documentation for book allocation problem

---
+++ DSA/array/bookAllocation.js
@@ -0,0 +1,17 @@
+/** Given an array arr[] of integers, where each element arr[i] represents the number of pages in the i-th book. You also have an integer k representing the number of students. The task is to allocate books to each student such that:
+
+Each student receives atleast one book.
+Each student is assigned a contiguous sequence of books.
+No book is assigned to more than one student.
+All books must be allocated.
+The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum. If it is not possible to allocate books to all students, return -1;
+
+*/
+
+let arr = [12, 34, 67, 90];
+const k = 2;
+
+function findPages(arr,k){
+
+    
+}
